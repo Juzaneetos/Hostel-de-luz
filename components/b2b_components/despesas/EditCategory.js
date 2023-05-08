c7c8cf4587b24ccb6d59d6f942a90dd77c69885e
Juzaneetos <129681939+Juzaneetos@users.noreply.github.com>
2023-05-08 09:28:41 -0300

    Update EditCategory.js

--- components/b2b_components/despesas/EditCategory.js
+++ components/b2b_components/despesas/EditCategory.js
@@ -89,7 +89,7 @@ function EditCategory({ despesasId, despesas, setShowEditCategoryComponent }) {
                 <option value="geral">geral</option>
                 {allcategorias?.map((item, index) => {
                   return(
-                    <option value={item._id}>{item.titulo}</option>
+                    <option key={item._id} value={item._id}>{item.titulo}</option>
                   )
                 })}
               </select>
@@ -104,7 +104,7 @@ function EditCategory({ despesasId, despesas, setShowEditCategoryComponent }) {
                 <option value="geral">geral</option>
                 {allhostels?.map((item, index) => {
                   return(
-                    <option value={item._id}>{item.titulo}</option>
+                    <option key={item._id} value={item._id}>{item.titulo}</option>
                   )
                 })}
               </select>
